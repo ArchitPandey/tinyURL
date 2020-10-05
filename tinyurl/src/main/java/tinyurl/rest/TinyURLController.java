3bc5734ae7757a071eb4ebb7454aa0c17e67762e
Archit Pandey <cpande91@gmail.com>
2020-10-05 09:52:50 +0530

    Remove unused imports

--- tinyurl/src/main/java/tinyurl/rest/TinyURLController.java
+++ tinyurl/src/main/java/tinyurl/rest/TinyURLController.java
@@ -1,7 +1,5 @@
 package tinyurl.rest;
 
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import tinyurl.commons.TinyURLException;
